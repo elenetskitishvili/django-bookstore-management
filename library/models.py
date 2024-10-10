0c694b168b9f2e5def314377cdf5f8f590d72187
elenetskitishvili <elene.tskitishvili504@gmail.com>
2024-10-10 13:59:13 +0400

    feat(models, templates): update models and HTML templates to display author books and student courses

--- library/models.py
+++ library/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 
 class Author(models.Model):
@@ -7,20 +8,23 @@ class Author(models.Model):
     def __str__(self) -> str:
         return str(self.name)
 
+    def number_of_books(self):
+        return self.book_set.count()
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-    def __str__(self) -> str:
-        return str(self.title)
+    def __str__(self):
+        return self.title
 
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
 
-    def __str__(self) -> str:
-        return str(self.name)
+    def __str__(self):
+        return self.name
 
 
 class Student(models.Model):
@@ -28,8 +32,16 @@ class Student(models.Model):
     courses = models.ManyToManyField(Course)
     date_of_birth = models.DateField()
 
-    def __str__(self) -> str:
-        return str(self.name)
+    def __str__(self):
+        return self.name
 
     def list_courses(self):
         return ', '.join(course.name for course in self.courses.all())
+
+    @property
+    def age(self):
+        today = date.today()
+        return today.year - self.date_of_birth.year - (
+            (today.month, today.day) < (
+                self.date_of_birth.month, self.date_of_birth.day)
+        )
